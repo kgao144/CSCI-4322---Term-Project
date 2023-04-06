67af2291c252a96e84a769070f5f8b36e120c3f7
Grady-Cliche <100376049+Grady-Cliche@users.noreply.github.com>
2023-04-05 22:23:45 -0400

    Updated UI

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import eliza
-import tkinter
+import tkinter as tk
 from tkinter import ttk
 from pynput import keyboard
 
@@ -8,9 +8,21 @@ eliza.load('doctor.txt')
 
 
 
-root = tkinter.Tk()
+root = tk.Tk()
 root.title("Eliza the Assitant")
-root.geometry('1200x800')
+root.geometry('630x830')
+
+toolbar_frame = tk.Frame(root, width=630, height=200)
+toolbar_frame.grid(row=0,column=0, sticky='ne')
+
+output_frame = tk.Frame(root, width=630, height=600)
+output_frame.grid(row=1,column=0,pady=5,padx=5)
+tk.Label(output_frame, text="Eliza",font=('Small Fonts',60)).grid(row=0, column=0)
+
+input_frame = tk.Frame(root, width=630, height=200)
+input_frame.grid(row=2, column=0,padx=10,pady=10)
+
+
 
 style = ttk.Style()
 style.configure('Custom.TEntry', borderwidth=0, relief='flat',
@@ -49,29 +61,33 @@ def change_theme():
 def on_press(key):
     if key == keyboard.Key.enter:
         submit()
-    
+
 listener = keyboard.Listener(
     on_press=on_press)
 listener.start()
 
-button = ttk.Button(root, text="Submit", command=submit)
+button = ttk.Button(input_frame, text="Submit", command=submit)
 button.grid(row = 0, column = 0)
 
-button2 = ttk.Button(root, text="Change theme!", command=change_theme)
-button2.grid(row = 0, column= 1, pady=5, padx=5)
+button2 = ttk.Button(toolbar_frame, text="Change theme!", command=change_theme)
+button2.grid(row = 0, column= 1)
 
-text_box = tkinter.Text(root, relief='groove', height=30, width = 80, borderwidth=1)
-text_box.grid(row = 1, column = 0, pady=5, padx=5)
+
+text_box = tk.Text(output_frame, relief='groove', height=30, width = 80, borderwidth=2, pady=5, padx=5)
+text_box.grid(row = 1, column = 0, sticky='ne')
 text_box.insert("end-1c",'Eliza: ','ElizaNameTag')
 text_box.insert("end-1c",eliza.initial()+'\n')
 text_box.tag_config('ElizaNameTag', foreground="cyan")
 text_box.tag_config('YouNameTag', foreground="green")
 text_box.config(state= 'disabled')
 
-input_box = ttk.Entry(root, width=20,font=(60), style='Custom.TEntry')
-input_box.grid(row=1,column=2)
+input_box = ttk.Entry(input_frame, width=50,font=(40), style='Custom.TEntry')
+input_box.grid(row=0,column=1, pady=10, padx =10)
 
-
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(1, weight=1)
 
 
 root.mainloop()
