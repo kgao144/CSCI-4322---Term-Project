27e9588c46da833b9d89f833c31f8279fa23028f
kgao144 <kevin.gao144@gmail.com>
2023-04-06 19:13:28 -0400

    Update eliza_application.py

--- eliza_application.py
+++ eliza_application.py
@@ -2,6 +2,7 @@ import tkinter
 import datetime
 import eliza
 import tkinter as tk
+from tkinter import *
 from tkinter import ttk
 from pynput import keyboard #pip install pynput
 
@@ -10,7 +11,7 @@ eliza = eliza.Eliza()
 # loads doctor.txt for response filtering
 eliza.load('doctor.txt')
 
-# initializes a Tkinter application window and name it 'Eliza the Assistant' at 630x830 pixels
+# initializes a window with title and geometry
 root = tk.Tk()
 root.title("Eliza the Assitant")
 root.geometry('630x830')
@@ -39,6 +40,7 @@ style.configure('Custom.TEntry', borderwidth=0, relief='flat',
 root.tk.call("source", "azure.tcl")
 root.tk.call("set_theme", "dark")
 
+
 # submit function, includes state of textbox as well as Eliza responses and datetime feature 
 def submit():
     # turns the tkinter text widget interactive
@@ -48,20 +50,35 @@ def submit():
         return
     response = eliza.respond(input_txt)
     now = datetime.datetime.now()
-    text_box.insert("end-1c",now.strftime("%I:%M%p |")+' You: ','YouNameTag')
+    text_box.insert("end-1c",now.strftime('\n'+"%I:%M%p |")+' You: ','YouNameTag')
     text_box.insert("end-1c",input_txt+'\n')
     now = datetime.datetime.now()
     text_box.insert("end-1c",now.strftime('%I:%M%p |')+' Eliza: ', 'ElizaNameTag')
-    text_box.insert("end-1c", response+'\n')
+    text_box.insert("end-1c", response)
     text_box.see(tkinter.END)
     # turns the tkinter text widget to 'readonly'
     text_box.config(state='disabled')
-    clearInputField()
+    input_box.delete(0,"end")
+    
+# on_press method where if input key is 'enter' calls submit function
+def on_press(key):
+    if key == keyboard.Key.enter:
+        submit()
+
+listener = keyboard.Listener(
+    on_press=on_press)
+listener.start()
+
+
+# close window function
+def close():
+    root.quit()
 
 # clear input text function
 def clearInputField():
     input_box.delete("0","end")
-                     
+
+
 # change_theme function 
 def change_theme():
     # NOTE: The theme's real name is azure-<mode>
@@ -73,15 +90,6 @@ def change_theme():
         # Set dark theme
         root.tk.call("set_theme", "dark")
         text_box.config(bg="#A2A2A2")
-
-# on_press method where if input key is 'enter' calls submit function
-def on_press(key):
-    if key == keyboard.Key.enter:
-        submit()
-
-listener = keyboard.Listener(
-    on_press=on_press)
-listener.start()
 
 # creates Submit button
 button = ttk.Button(input_frame, text="Submit", command=submit)
@@ -95,10 +103,10 @@ button2.grid(row = 0, column= 1)
 text_box = tk.Text(output_frame, relief='groove', height=30, width = 80, borderwidth=2, pady=5, padx=5, bg='#A2A2A2')
 text_box.grid(row = 1, column = 0, sticky='ne')
 
-# text box content parameters
+# text box beginning content parameters
 now = datetime.datetime.now()
 text_box.insert("end-1c",now.strftime('%I:%M%p |')+' Eliza: ','ElizaNameTag')
-text_box.insert("end-1c",eliza.initial()+'\n')
+text_box.insert("end-1c",eliza.initial())
 text_box.tag_config('ElizaNameTag', foreground="black")
 text_box.tag_config('YouNameTag', foreground="green")
 text_box.config(state= 'disabled')
